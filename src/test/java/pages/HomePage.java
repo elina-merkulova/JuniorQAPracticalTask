2656548fc5414269f87f4209fe35c219d09fdd13
Vidro4ka <test@test.lv>
2022-11-10 15:14:33 +0200

    Final commit

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class HomePage {
     private final By ACCEPT_COOKIES_BTN = By.id("c-p-bn");
     private final By OPEN_SIDE_MENU = By.xpath(".//button[@class = 'btn btn-empty header-icon tw-mr-2 tw-hidden tw-self-center tw-rounded tw-p-1.5 tw-leading-none tw-outline-none hover:tw-bg-purple-dark hover:tw-text-white focus:tw-bg-purple-dark focus:tw-text-white xl:tw-block']");
-    private final By INCREASING_RANK = By.xpath(".//fa-icon[@class = 'ng-fa-icon tw-text-base tw-text-green']");
+    private final By INCREASING_RANK = By.xpath("//fa-icon[@class = 'ng-fa-icon tw-text-base tw-text-green']");
     private final By TOP_GAMES = By.xpath(".//li[@class = 'hotness-item']");
     private BaseFunk baseFunk;
 
@@ -28,7 +28,7 @@ public class HomePage {
 
         List<WebElement> topGames = baseFunk.findElements(TOP_GAMES);
         for (WebElement game : topGames) {
-            baseFunk.click(INCREASING_RANK);
+            game.findElement(INCREASING_RANK).click();
             break;
             }
         }
